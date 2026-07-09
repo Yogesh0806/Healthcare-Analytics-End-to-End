2f839ed567d23f8227bcc236ca6efcd9b16d99f7
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-07-09 23:19:21 +0530

    Add raw healthcare dataset

---
+++ src/Data_overview.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+df = pd.read_csv(r'C:\Users\HP\OneDrive\Documents\Desktop\Project-Healthcare-Analysis\Dataset\healthcare_dataset.csv')
+print(df.head())
